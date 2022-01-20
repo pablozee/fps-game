8f4668c4158e6a4cfc7d33fa5bfb9bc6627e2ca0
Zeshan Rasul <zeshanrasul@gmail.com>
2022-01-20 15:34:35 +0000

    Player movement and gravity wip

---
+++ fps-game/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller;
+    [SerializeField]
+    private float speed = 12f;
+    [SerializeField]
+    private float gravity = -9.81f;
+
+    private Vector3 velocity;
+
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
